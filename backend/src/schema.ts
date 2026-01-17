2964e24758840fa0f1063f1b26e4ddd0eba2dc04
symoleon <35866977+symoleon@users.noreply.github.com>
2026-01-17 22:20:18 +0100

    Eksport typów schematów zoda

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -7,9 +7,11 @@ export const addressSchema = z.object({
     postal_code: z.string(),
     apartment_no: z.string().optional(),
 });
+export type Address = z.infer<typeof addressSchema>;
 
 export const shipmentSchema = z.object({
     recipientAddress: addressSchema,
     recipientPhone: z.string(),
     size: z.enum(["S", "M", "L", "XL"]),
 });
+export type Shipment = z.infer<typeof shipmentSchema>;
