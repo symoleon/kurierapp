9d36532a264384c92a1d356cfd9ee14ecb7f3ff4
symoleon <35866977+symoleon@users.noreply.github.com>
2026-01-17 22:20:21 +0100

    Usunięcie roboczego loga

--- backend/src/api/user.ts
+++ backend/src/api/user.ts
@@ -7,7 +7,6 @@ const router = Router();
 router.get('/@current', async (req, res) => {
     const authReq = req as AuthRequest;
     const user = await getUserById(authReq.userId);
-    console.log(user);
     if (!user) {
         return res.sendStatus(403);
     }
