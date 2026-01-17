5ef4c955035dfb3583bb17900a69e1713f94e92d
symoleon <35866977+symoleon@users.noreply.github.com>
2026-01-17 22:20:16 +0100

    Utworzenie endpointu POST /api/shipment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import z from 'zod';
+import { createShipmentSchema } from "./schema";
+
+const formatErrors = (error: z.ZodError): string[] => {
+    return error.issues.map((issue) => {
+        return `${issue.path.join('.')}: ${issue.message}`;
+    });
+};
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/api/shipments', (req, res) => {
+   const result = createShipmentSchema.safeParse(req.body);
+   if (!result.success) {
+       res.status(400).json(formatErrors(result.error));
+       return;
+   }
+   res.json(result.data);
 });
 
 app.listen(port, () => {
