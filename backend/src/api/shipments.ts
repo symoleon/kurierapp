bbe3d3f05e29ff2f04be1824f4bdbfb243e204bf
symoleon <35866977+symoleon@users.noreply.github.com>
2026-01-17 22:20:23 +0100

    Naprawienie warunku sprawdzającego przynależność przesyłki

--- backend/src/api/shipments.ts
+++ backend/src/api/shipments.ts
@@ -7,7 +7,7 @@ router.get('/:id', async (req, res) => {
     const authReq = req as unknown as AuthRequest;
     const shipment = await getShipment(req.params.id);
     if (!shipment) return res.sendStatus(403);
-    if (shipment.recipientId != authReq.userId || shipment.senderId != authReq.userId) return res.sendStatus(403);
+    if (shipment.recipientId != authReq.userId && shipment.senderId != authReq.userId) return res.sendStatus(403);
     return res.status(200).send(shipment);
 });
 
