7ef431ce5ceb434a6f72167f83213ad2648c8f0d
symoleon <35866977+symoleon@users.noreply.github.com>
2026-01-17 22:20:18 +0100

    Poprawa kolejności tworzenia tabel

--- db_init.sql
+++ db_init.sql
@@ -1,5 +1,16 @@
 CREATE TYPE shipment_size AS ENUM ('S', 'M', 'L', 'XL');
 
+CREATE TABLE users(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    postal_code VARCHAR NOT NULL,
+    street VARCHAR,
+    building_no VARCHAR NOT NULL,
+    apartment_no VARCHAR
+);
+
 CREATE TABLE shipments(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     sender_id uuid NOT NULL REFERENCES users(id),
