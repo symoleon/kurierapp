a34dbd31d081c822778f247080ec7126eb825d5d
symoleon <35866977+symoleon@users.noreply.github.com>
2026-01-17 22:20:16 +0100

    Dodanie podstawowych typów

---
+++ backend/src/schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const addressSchema = z.object({
+    city: z.string(),
+    street: z.string().optional(),
+    building_no: z.string(),
+    postal_code: z.string(),
+    apartment_no: z.string().optional(),
+});
+
+export const createShipmentSchema = z.object({
+    recipientAddress: addressSchema,
+    recipientPhone: z.string(),
+    size: z.enum(["S", "M", "L", "XL"]),
+});
